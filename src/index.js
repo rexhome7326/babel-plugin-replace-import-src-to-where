41976b4128765e486fa2e71e8a430c921b9c29f2
rex.chen <rex.chen@104.com.tw>
2016-09-26 10:43:47 +0800

    remove flag '--replace-word'@0.0.3

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 "use strict";
 
 exports.default = function (babel) {
-  var checkFlag = process.argv.slice(-1).pop() === "--replace-word";
   var source =  new RegExp("^src\\/", "gi");
   var replaceTo = "lib/";
 
   return {
     visitor: {
       ImportDeclaration(path, state) {
-        if(checkFlag === true){
-          if(source.test(path.node.source.value)){
-            path.node.source.value = path.node.source.value.replace(source, replaceTo);
-          }
+        if(source.test(path.node.source.value)){
+          path.node.source.value = path.node.source.value.replace(source, replaceTo);
         }
       },
       CallExpression(path, state) {
-        if(checkFlag === true && path.node.arguments.length > 0){
+        if(path.node.arguments.length > 0){
           path.node.arguments.map(function loop(node, index){
             if(node.type === "StringLiteral" && source.test(node.value)){
               node.value = node.value.replace(source, replaceTo);
@@ -24,7 +21,7 @@ exports.default = function (babel) {
         }
       },
       ArrayExpression(path, state) {
-        if(checkFlag === true && path.node.elements.length > 0){
+        if(path.node.elements.length > 0){
           path.node.elements.map(function loop(node, index){
             if(node.type === "StringLiteral" && source.test(node.value)){
               node.value = node.value.replace(source, replaceTo);
@@ -33,10 +30,8 @@ exports.default = function (babel) {
         }
       },
       ObjectProperty(path, state) {
-        if(checkFlag === true){
-          if(path.node.value.type === "StringLiteral" && source.test(path.node.value.value)){
-            path.node.value.value = path.node.value.value.replace(source, replaceTo);
-          }
+        if(path.node.value.type === "StringLiteral" && source.test(path.node.value.value)){
+          path.node.value.value = path.node.value.value.replace(source, replaceTo);
         }
       }
     }
